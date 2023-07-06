3b1e01821bd5c4b934e8c9ac6b9826d761582929
ankittkinha <ankittkinha@gmail.com>
2023-07-06 22:17:09 +0530

    formatted code

--- index.js
+++ index.js
@@ -41,31 +41,31 @@ heading1.addEventListener("click", function (e) {
     document.querySelector(".btnDesign2").setAttribute("style", "display: none !important");
     document.querySelector(".btnDesign3").setAttribute("style", "display: none !important");
 
-})
+});
 
 heading2.addEventListener("click", function (e) {
     e.stopPropagation();
 
     document.querySelector(".dropdown1").setAttribute("style", "display: none !important");
-    document.querySelector(".dropdown2").style.display = "block"
+    document.querySelector(".dropdown2").style.display = "block";
     document.querySelector(".dropdown3").setAttribute("style", "display: none !important");
 
     document.querySelector(".btnDesign1").setAttribute("style", "display: none !important");
-    document.querySelector(".btnDesign2").style.display = "block"
+    document.querySelector(".btnDesign2").style.display = "block";
     document.querySelector(".btnDesign3").setAttribute("style", "display: none !important");
-})
+});
 
 heading3.addEventListener("click", function (e) {
     e.stopPropagation();
 
     document.querySelector(".dropdown1").setAttribute("style", "display: none !important");
     document.querySelector(".dropdown2").setAttribute("style", "display: none !important");
-    document.querySelector(".dropdown3").style.display = "block"
-
+    document.querySelector(".dropdown3").style.display = "block";
+;
     document.querySelector(".btnDesign1").setAttribute("style", "display: none !important");
     document.querySelector(".btnDesign2").setAttribute("style", "display: none !important");
-    document.querySelector(".btnDesign3").style.display = "block"
-})
+    document.querySelector(".btnDesign3").style.display = "block";
+});
 
 window.addEventListener("click", function () {
     document.querySelector(".dropdown1").style.display = "none";
